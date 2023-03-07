1b937e93949c6217e07824b8f4095058e3402acd
Koval Dmytro <97359964+DmitryK0v@users.noreply.github.com>
2023-03-07 20:14:00 +0200

    Create Models Ads and AdditionalImage, migrate

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .utilities import get_timestamp_path
 
 
 # Create your models here.
@@ -53,3 +54,34 @@ class SubRubric(Rubric):
         ordering = ('super_rubric__order', 'super_rubric__name', 'order', 'name')
         verbose_name = 'Sub Rubric'
         verbose_name_plural = 'Sub Rubrics'
+
+
+class Ads(models.Model):
+    rubric = models.ForeignKey(SubRubric, on_delete=models.PROTECT, verbose_name='Rubric')
+    title = models.CharField(max_length=40, verbose_name=' Goods')
+    content = models.TextField(verbose_name='Description')
+    price = models.FloatField(default=0, verbose_name='Price')
+    contacts = models.TextField(verbose_name='contacts')
+    image = models.ImageField(blank=True, upload_to=get_timestamp_path, verbose_name='Image')
+    author = models.ForeignKey(AdvUser, on_delete=models.CASCADE, verbose_name="Ad author")
+    is_active = models.BooleanField(default=True, db_index=True, verbose_name='Display in a list?')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published')
+
+    def delete(self, *args, **kwargs):
+        for ai in self.additionalimage_set.all():
+            ai.delete()
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Ads'
+        verbose_name = 'Ad'
+        ordering = ['-created_at']
+
+
+class AdditionalImage(models.Model):
+    ads = models.ForeignKey(Ads, on_delete=models.CASCADE, verbose_name='Ad')
+    image = models.ImageField(upload_to=get_timestamp_path, verbose_name='Image')
+
+    class Meta:
+        verbose_name_plural = 'Additional illustrations'
+        verbose_name = 'Additional illustrate'
